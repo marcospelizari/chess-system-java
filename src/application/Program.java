33a512587a43da2f8d553b1a941ad3ed1feed28f
Marcos <132465567+marcospelizari@users.noreply.github.com>
2025-12-23 15:59:25 -0300

    First commit

---
+++ src/application/Program.java
@@ -0,0 +1,10 @@
+package application;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Program {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
